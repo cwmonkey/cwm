91f6d39563f1d933b7d98cceade3bc3a42758fac
cwmonkey <wackoburns@hotmail.com>
2013-09-07 05:15:27 -0700

    updating messages to pop up at specific locations.

--- message/jquery.message.js
+++ message/jquery.message.js
@@ -1,12 +1,18 @@
-(function($) {
+(function(window, $, undefined) {
 
-var defaults = {
-	message: '&nbsp;',
-	node: 'body',
-	prefix: 'message_'
-};
+var $window = $(window);
 
 $.message = function(args) {
+	var defaults = {
+		message: '&nbsp;',
+		node: window,
+		container: 'body',
+		prefix: 'message_',
+		activeTime: 3000,
+		removeAfter: 1000,
+		addClass: ''
+	};
+
 	if ( typeof args == 'string' || typeof args == 'number' ) {
 		args = {message: args};
 	}
@@ -15,54 +21,65 @@ $.message = function(args) {
 
 	var msg = opts.message;
 console.log(msg);
-	var $node = opts.node;
-	if ( !$node.jQuery ) $node = $($node);
+	var $node = $(opts.node);
 	if ( !$node[0] ) return;
+
+	var $container = $(opts.container);
+	if ( !$container[0] ) return;
 
 	var prefix = opts.prefix;
 
-	var adjust_margins = function($nodes) {
-		var margin = 0;
-		$node.find('.' + prefix + 'message.' + prefix + 'active').each(function() {
-			var $this = $(this);
-			$this.css('margin-top', margin);
-			margin += $this.outerHeight();
-		});
-	};
-
+	var messages = $node.data('messages') || [];
 	var hide = function($message) {
+		var $m;
+		for ( var i = 0; i < messages.length; i++ ) {
+			var m = messages[i];
+			if ( m == $message[0] ) {
+				messages.splice(i, 1);
+				break;
+			}
+		}
 		$message.removeClass(prefix + 'active').addClass(prefix + 'inactive');
-		setTimeout(function() { /*$message.remove();*/ }, 1000);
-		adjust_margins();
+		if ( opts.removeAfter ) {
+			setTimeout(function() { /*$message.remove(); delete $message;*/ }, opts.removeAfter);
+		}
 	};
 
-	var get_heights = function() {
-		var margin = 0;
-		$node.find('.' + prefix + 'message.' + prefix + 'active').each(function() {
-			var $this = $(this);
-			margin += $this.outerHeight();
-		});
-		return margin;
-	};
+	var $p = $('<div/>').addClass(prefix + 'message').addClass(opts.addClass).append('<p>' + msg + '</p>');
+	$container.append($p);
 
-	var $p = $('<p>' + msg + '</p>').addClass(prefix + 'message');
-	$node.append($p);
+	var show_delay = opts.activeTime * messages.length;
+	messages.push($p[0]);
+	$node.data('messages', messages);
 
 	setTimeout(function() {
+		var node_is_window = $node[0] === window;
+		var message_height = $p.height();
+		var message_width = $p.width();
+		var top;
+		var left;
+
+		if ( node_is_window ) {
+			top = $window.height() / 2 - message_height / 2;
+			left = $window.width() / 2 - message_width / 2;
+		} else {
+			var offset = $node.offset();
+			top = offset.top + $node.height() / 2 - message_height / 2 - $window.scrollTop();
+			left = offset.left + $node.width() / 2 - message_width / 2 - $window.scrollLeft();
+		}
+
 		$p
-			.height($p.height())
-			.width($p.width())
 			.css({
-				marginTop: get_heights(),
-				marginLeft: -1 * $p.outerWidth() / 2
+				top: top,
+				left: left
 			})
 			.addClass(prefix + 'active')
 			;
-	}, 0);
 
-	setTimeout(function() {
-		hide($p);
-	}, 3000);
+		setTimeout(function() {
+			hide($p);
+		}, opts.activeTime);
+	}, show_delay);
 };
 
 $.fn.message = function(args) {
@@ -79,4 +96,4 @@ $.fn.message = function(args) {
 	});
 };
 
-})(jQuery);
+})(window, jQuery);
