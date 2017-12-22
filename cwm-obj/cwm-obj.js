5ddc3b703b28a6dc3ecb5cdacf146de03c2d13a7
cwmonkey <wackoburns@hotmail.com>
2017-12-21 18:35:31 -0800

    cwm obj extending

--- cwm-obj/cwm-obj.js
+++ cwm-obj/cwm-obj.js
@@ -1,6 +1,8 @@
 ;(function(undefined) {
+'use strict';
 
 var cwmo = {};
+cwmo.objs = {};
 
   /////////////////////////////
  // Events
@@ -16,6 +18,79 @@ cwmo.say = function(type) {
 
 		cwmo.listener.apply(window, args);
 	}
+};
+
+  /////////////////////////////
+ // Export
+/////////////////////////////
+
+cwmo.export = function() {
+	var obj = {};
+
+	for ( var id in cwmo.objs ) {
+		if ( cwmo.objs.hasOwnProperty(id) ) {
+			obj[id] = cwmo.objs[id].export();
+		}
+	}
+
+	return obj;
+};
+
+  /////////////////////////////
+ // Util
+/////////////////////////////
+
+// https://stackoverflow.com/questions/6213227/fastest-way-to-convert-a-number-to-radix-64-in-javascript
+var str = '';
+for ( var i = 33; i <= 126; i++ ) {
+	str += String.fromCharCode(i);
+}
+
+for ( i = 160; i <= 255; i++ ) { // 767
+	str += String.fromCharCode(i);
+}
+
+var lim = str.length;
+
+cwmo.encode = function(number) {
+  if (isNaN(Number(number)) || number === null ||
+    number === Number.POSITIVE_INFINITY)
+    throw "The input is not valid";
+  if (number < 0)
+    throw "Can't represent negative numbers now";
+
+  var rixit; // like 'digit', only in some non-decimal radix 
+  var residual = Math.floor(number);
+  var result = '';
+  while (true) {
+    rixit = residual % lim;
+    // console.log("rixit : " + rixit);
+    // console.log("result before : " + result);
+    result = str.charAt(rixit) + result;
+    // console.log("result after : " + result);
+    // console.log("residual before : " + residual);
+    residual = Math.floor(residual / lim);
+    // console.log("residual after : " + residual);
+
+    if (residual === 0)
+      break;
+    }
+  return result;
+};
+
+cwmo.decode = function(rixits) {
+  var result = 0;
+  // console.log("rixits : " + rixits);
+  // console.log("rixits.split('') : " + rixits.split(''));
+  rixits = rixits.split('');
+  for (var e = 0; e < rixits.length; e++) {
+    // console.log("_Rixits.indexOf(" + rixits[e] + ") : " + 
+        // this._Rixits.indexOf(rixits[e]));
+    // console.log("result before : " + result);
+    result = (result * lim) + str.indexOf(rixits[e]);
+    // console.log("result after : " + result);
+  }
+  return result;
 };
 
   /////////////////////////////
@@ -37,7 +112,10 @@ cwmo.Class = function(params) {
 			for ( var i = 0, attr; (attr = this.constructor.__has[i]); i++ ) {
 				this.__hases.push(attr);
 
+				this.__hasesN[attr.name] = attr;
+
 				if ( attr.alias ) {
+					if ( this.__hasesAtoN[attr.alias] ) throw 'Error: Already has attr alias';
 					this.__hasesAtoN[attr.alias] = attr.name;
 				}
 			}
@@ -45,22 +123,28 @@ cwmo.Class = function(params) {
 			this.__attrsAdded = true;
 		}
 
-		// Call object's constructor
-		target.apply(this, arguments);
-
 		// If this constructor was the one called, run the end-of-chain methods
 		if ( this.constructor === fn ) {
+			for ( var i = 0, attr; (attr = attrs[i]); i++ ) {
+				if ( typeof attr === 'string' ) {
+					attr = {name: attr};
+				}
+
+				this.__values[attr.name] = attr.default;
+			}
+
 			if ( imports ) {
 				this.import(imports);
 			}
+		}
 
+		// Call object's constructor
+		target.apply(this, arguments);
+
+		if ( this.constructor === fn ) {
 			cwmo.say('add', this.__type, this.__id, this.serialize());
 		}
 	};
-
-	// Add attributes
-	fn.__has = base.__has || [];
-	fn.__has.push.apply(fn.__has, attrs);
 
 	// Set fn's prototype to base's prototype
 	fn.prototype = Object.create(base.prototype);
@@ -68,18 +152,29 @@ cwmo.Class = function(params) {
 	// Set constructor back to fn
 	fn.prototype.constructor = fn;
 
-	// Prototype attr functions
-	// TODO: allow for custom get/set functions?
+	fn.__has = base.__has || [];
+
 	for ( var i = 0, attr; (attr = attrs[i]); i++ ) {
+		// Add attributes
+		if ( typeof attr === 'string' ) {
+			attr = {name: attr};
+		}
+
+		fn.__has.push(attr);
+
+		// Prototype attr functions
+		// TODO: allow for custom get/set functions?
 		(function(name) {
-			fn.prototype[name] = function(val, what) {
+			fn.prototype[name] = function(val, what, why) {
 				if ( val === undefined ) {
 					return this.__values[name];
 				}
 
 				this.__values[name] = val;
 
-				cwmo.say('val', this.__id, name, val, what);
+				what = what ? what.__id : undefined;
+
+				cwmo.say('val', this.__id, name, val, what, why);
 
 				return this.__values[name];
 			};
@@ -94,14 +189,25 @@ cwmo.Class = function(params) {
 /////////////////////////////
 
 // To have a unique id on every object created
-var idcur = Date.now();
+cwmo.idCur = 1;
 
-cwmo.__baseClass = function() {
+cwmo.__baseClass = function(params) {
+	this.__isCwmo = true;
 	this.__values = {};
 	this.__hases = [];
 	this.__hasesAtoN = {}; // alias to name lookup
-	this.__id = idcur++;
+	this.__hasesN = {}; // to see if name exists
 	this.__attrsAdded = false;
+
+	if ( params && params.__id ) {
+		this.__id = params.__id;
+	} else {
+		this.__id = cwmo.idCur++;
+	}
+
+	if ( cwmo.objs[this.__id] ) throw 'Error: Object ID exists';
+
+	cwmo.objs[this.__id] = this;
 };
 
 // Serialize object data using names
@@ -119,12 +225,36 @@ cwmo.__baseClass.prototype.serialize = function() {
 // Serialize object data using aliases
 cwmo.__baseClass.prototype.export = function() {
 	var obj = {};
+	var val;
+	var j, jl, jv;
+
 	for ( var i = 0, has; (has = this.__hases[i]); i++ ) {
 		if ( has.alias ) {
-			if ( this.__values[has.name] !== undefined ) {
-				obj[has.alias] = this.__values[has.name];
+			val = this.__values[has.name]; 
+			if ( val !== undefined ) {
+				if ( Array.isArray(val) ) {
+					obj[has.alias] = [];
+
+					for ( j = 0, jl = val.length; j < jl; j++ ) {
+						jv = val[j];
+
+						if ( jv.__isCwmo ) {
+							obj[has.alias].push(jv.__id);
+						} else {
+							obj[has.alias].push(jv);
+						}
+					}
+				} else if ( val.__isCwmo ) {
+					obj[has.alias] = val.__id;
+				} else {
+					obj[has.alias] = val;
+				}
 			}
 		}
+	}
+
+	if ( this.__type ) {
+		obj._t = this.__type;
 	}
 
 	return obj;
@@ -132,12 +262,49 @@ cwmo.__baseClass.prototype.export = function() {
 
 // Import json data with aliases to object (do not fire hooks)
 cwmo.__baseClass.prototype.import = function(obj) {
+	var name;
 	for ( var prop in obj ) {
 		if ( obj.hasOwnProperty(prop) ) {
-			this.__values[this.__hasesAtoN[prop]] = obj[prop];
+			name = null;
+
+			if ( this.__hasesAtoN[prop] ) {
+				name = this.__hasesAtoN[prop];
+			} else if ( this.__hasesN[prop] ) {
+				name = prop;
+			}
+
+			if ( name ) {
+				this.__values[name] = obj[prop];
+			}
 		}
 	}
 };
+
+/*// Replace cwmo references with actual objects
+cwmo.__baseClass.prototype.init = function(obj) {
+	var name;
+	for ( var prop in obj ) {
+		if ( obj.hasOwnProperty(prop) ) {
+			name = null;
+
+			if ( this.__hasesAtoN[prop] ) {
+				name = this.__hasesAtoN[prop];
+			} else if ( this.__hasesN[prop] ) {
+				name = prop;
+			}
+
+			if ( name ) {
+				if ( this.__hasesN[name].type === 'cwma' ) {
+					for ( var i = 0, id; (id = this.__values[name][i]); i++ ) {
+						this.__values[name][i] = cwmo.objs[id];
+					}
+				} else if ( this.__hasesN[name].type === 'cwmo' ) {
+					this.__values[name] = cwmo.objs[this.__values[name]];
+				}
+			}
+		}
+	}
+};*/
 
 window.cwmo = cwmo;
 
